9cb399bbc8a8041d443e6e4da880cd679d32a959
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-01-15 13:14:57 -0600

    Add files via upload

---
+++ phpgo.php
@@ -0,0 +1,17 @@
+<!docutype HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>PHP practice</title>
+</head>
+<body>
+<?php
+	echo "<h1>HELLO WORLD!!</h1>"
+?>
+<h1>First Paragraph</h1>
+<?php
+	print "<h2>Here is a line of PHP</h2>"
+?>
+<p>jkjkjk</p>
+</body>
+</HTML>
